706e788c78a60e6a755ddb695106f69906180498
Nate Swanson <nate.swanson@ave81.com>
2015-08-02 12:04:03 -0500

    Add in a bit of code for if the client flow hasn't executed. Probably doesn't work.

--- gcalhue/application_base.py
+++ gcalhue/application_base.py
@@ -14,7 +14,6 @@ from resources import CalendarResource
 import phue
 
 
-
 class Application(object):
     def __init__(self, prefs, logger):
         self.prefs = prefs
@@ -97,13 +96,16 @@ class Application(object):
 
         store = oauth2client.file.Storage(credential_path)
         credentials = store.get()
+        SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+        CLIENT_SECRET_FILE = 'client_secret.json'
         if not credentials or credentials.invalid:
             flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-            flow.user_agent = APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else: # Needed only for compatability with Python 2.6
-                credentials = tools.run(flow, store)
+            flow.user_agent = "Gcal-Hue"
+            # if flags:
+            #     credentials = tools.run_flow(flow, store, flags)
+            # else: # Needed only for compatability with Python 2.6
+            #     credentials = tools.run(flow, store)
+            credentials = tools.run(flow, store)
             print 'Storing credentials to ' + credential_path
         return credentials
 
